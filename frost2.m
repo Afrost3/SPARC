d3de7cfe488bd6e92881004cbbd47fb97e860733
Andrew Frost <Afrost3@student.gsu.edu>
2013-06-24 08:37:01 -0400

    Added comments

--- frost2.m
+++ frost2.m
@@ -1,48 +1,51 @@
+!Andrew Frost
+!Assignment 2
+!Dr. Anu Bourgeois
 	.section ".data"
-define(xmax,10)
-define(a0,3)
+define(xmax,10)  !This is max value for x which is used in the loop
+define(a0,3)     !These are the values for integers in the equation
 define(a1,40)
 define(a2,15)
 
-define(x_r,l0)
-define(y_r,l1)
-define(z_r,l2)
+define(x_r,l0)   !This is where the value of x is stored
+define(y_r,l1)   !This is where I store the values for y currently
+define(z_r,l2)	 !This is used to store the current sum of the equation
 
 .global main
 main:	
 	save %sp,-96,%sp
-	mov 1,%x_r
-	clr %z_r
-loop:	mov %x_r,%o0
-	call .mul
-	mov %x_r,%o1
-	call .mul
-	mov a0,%o1
-	mov %o0,%y_r
-	mov a1,%o0
-	call .mul
-	mov %x_r,%o1
-	sub %o0,a2,%o0
-	add %y_r,%o0,%y_r
-	add %z_r,%y_r,%z_r
+	mov 1,%x_r   		!Setting x=1
+	clr %z_r		!Clearing out z so that it is sure to be equal to 0
+loop:	mov %x_r,%o0            !Start of the loop and moving x to register o0
+	call .mul		!Calling the multiply before moving the value to o1 to save a nop
+	mov %x_r,%o1		!This is how the x^2 is obtained
+	call .mul		!Multiplying x^2 by the first constant again with the mov after mul
+	mov a0,%o1		
+	mov %o0,%y_r		!Now have y=ax^2 stored in the y_r register
+	mov a1,%o0		!Moving the second constant for multiplying
+	call .mul		!Same as before calling the mul before the seond move
+	mov %x_r,%o1		!This moves x in so that we now have b*x
+	sub %o0,a2,%o0		!Subtracting c from the previous answer and placing it back in the register
+	add %y_r,%o0,%y_r	!Now adding the previous answer to the y_r register and now have answer
+	add %z_r,%y_r,%z_r	!Adding the answer to z_r to update the sum 
 
-	set string,%o0
-	mov %x_r, %o1
+	set string,%o0		!This is to display the value for x string
+	mov %x_r, %o1		
 	mov %y_r,%o2
         call printf
 
-	inc %x_r
-	cmp %x_r,10
-	ble loop
-	nop
-	set string2,%o0
+	inc %x_r		!This is where x is incremented
+	cmp %x_r,10		!then compared
+	ble loop		!branch if less than
+	nop			!the only nop in the file, I didn't know if I could do without
+	set string2,%o0		!This is at the end to display the overall result from z_r
         call printf
 	mov %z_r, %o1
 
 	ret
 	restore
 string:
-.asciz "The value for x = %d is %d\n"
+.asciz "The value for x = %d is %d\n"	!String for displaying the solutions
 
 string2:
-.asciz "The overall result is y = %d\n"
+.asciz "The overall result is y = %d\n" !String for displaying the final sum
--- frost2.s
+++ frost2.s
@@ -1,48 +1,51 @@
+!Andrew Frost
+!Assignment 2
+!Dr. Anu Bourgeois
 	.section ".data"
+  !This is max value for x which is used in the loop
+     !These are the values for integers in the equation
 
 
 
-
-
-
-
-
+   !This is where the value of x is stored
+   !This is where I store the values for y currently
+	 !This is used to store the current sum of the equation
 
 .global main
 main:	
 	save %sp,-96,%sp
-	mov 1,%l0
-	clr %l2
-loop:	mov %l0,%o0
-	call .mul
-	mov %l0,%o1
-	call .mul
-	mov 3,%o1
-	mov %o0,%l1
-	mov 40,%o0
-	call .mul
-	mov %l0,%o1
-	sub %o0,15,%o0
-	add %l1,%o0,%l1
-	add %l2,%l1,%l2
+	mov 1,%l0   		!Setting x=1
+	clr %l2		!Clearing out z so that it is sure to be equal to 0
+loop:	mov %l0,%o0            !Start of the loop and moving x to register o0
+	call .mul		!Calling the multiply before moving the value to o1 to save a nop
+	mov %l0,%o1		!This is how the x^2 is obtained
+	call .mul		!Multiplying x^2 by the first constant again with the mov after mul
+	mov 3,%o1		
+	mov %o0,%l1		!Now have y=ax^2 stored in the l1 register
+	mov 40,%o0		!Moving the second constant for multiplying
+	call .mul		!Same as before calling the mul before the seond move
+	mov %l0,%o1		!This moves x in so that we now have b*x
+	sub %o0,15,%o0		!Subtracting c from the previous answer and placing it back in the register
+	add %l1,%o0,%l1	!Now adding the previous answer to the l1 register and now have answer
+	add %l2,%l1,%l2	!Adding the answer to l2 to update the sum 
 
-	set string,%o0
-	mov %l0, %o1
+	set string,%o0		!This is to display the value for x string
+	mov %l0, %o1		
 	mov %l1,%o2
         call printf
 
-	inc %l0
-	cmp %l0,10
-	ble loop
-	nop
-	set string2,%o0
+	inc %l0		!This is where x is incremented
+	cmp %l0,10		!then compared
+	ble loop		!branch if less than
+	nop			!the only nop in the file, I didn't know if I could do without
+	set string2,%o0		!This is at the end to display the overall result from l2
         call printf
 	mov %l2, %o1
 
 	ret
 	restore
 string:
-.asciz "The value for x = %d is %d\n"
+.asciz "The value for x = %d is %d\n"	!String for displaying the solutions
 
 string2:
-.asciz "The overall result is y = %d\n"
+.asciz "The overall result is y = %d\n" !String for displaying the final sum
